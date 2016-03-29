ae854e70c03c69b2064db600415ea4cfe0ed8b90
Adam Pash <adam.pash@gmail.com>
2016-03-29 14:37:52 -0400

    added config idk

--- config/prod.exs
+++ config/prod.exs
@@ -20,10 +20,10 @@ config :ghost, Ghost.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
-# config :ghost, Fire.Repo,
-#   adapter: Ecto.Adapters.Postgres,
-#   url: System.get_env("DATABASE_URL"),
-#   pool_size: 20
+config :ghost, Ghost.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
 
 # Do not print debug messages in production
 config :logger, level: :info
