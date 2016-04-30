b22385115b32700f889cdbba112ba090a8bb043e
Adam Pash <adam.pash@gmail.com>
2016-04-30 11:10:50 -0400

    updated prod config to use ssl by default

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :ghost, Ghost.Endpoint,
   http: [port: {:system, "PORT"}],
-  # url: [scheme: "https", host: System.get_env("HOSTNAME"), port: 443],
-  url: [host: System.get_env("HOSTNAME"), port: 80],
+  url: [scheme: "https", host: System.get_env("HOSTNAME"), port: 443],
+  # url: [host: System.get_env("HOSTNAME"), port: 80],
   # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
